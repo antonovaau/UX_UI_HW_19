f38027f266611fed35038cd1cc1c68e40535bd40
antonovaau <77912338+antonovaau@users.noreply.github.com>
2021-07-18 14:42:11 +1000

    testing1

---
+++ js/script.js
@@ -0,0 +1,28 @@
+console.log("Your index.js file i sloaded correctly");
+
+
+const toggleButton = document.getElementById('toggle-button');
+  const naviList = document.getElementById('navi-list');
+
+  toggleButton.addEventListener('click', () => {
+  naviList.classList.toggle('active');
+
+  });
+
+const scrollUp = document.querySelector('.scrollUp');
+
+window.addEventListener('scroll', () => {
+  if(window.pageYOffset > 100) {
+      scrollUp.classList.add ("active");
+  }else {
+      scrollUp.classList.remove("active");
+  }
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    behavior: 'smooth';
+  }
+});
+
+
